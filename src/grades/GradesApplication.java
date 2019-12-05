89f0576a48ba6c8e716ea2ada1cad5fd17c65b50
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-05 14:32:18 -0600

    printed out name and grade

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,11 +3,12 @@ import java.util.HashMap;
 import  java.util.ArrayList;
 import java.util.Scanner;
 
-public class GradesApplication {
+public class GradesApplication{
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //hashmap students
-        HashMap <String, String> students = new HashMap<>();
+        HashMap <String, Student> students = new HashMap<>();
 
         Student  Billy = new  Student<>("Billy");
         Student  Sophie = new  Student<>("Sophie");
@@ -32,29 +33,41 @@ public class GradesApplication {
         Viv.addGrade(67);
 
         //add to the map
-        students.put("Billy", "billy2");
-        students.put("Sophie", "sophiebeans");
-        students.put("Lance", "Lancealot");
-        students.put("Viv", "Vivjeez");
+        students.put("billy2", Billy);
+        students.put("sophiebeans", Sophie );
+        students.put("Lancealot", Lance);
+        students.put("Vivjeez", Viv);
+
+        ArrayList<String> keys = new ArrayList<>();
+        for(String userName : students.keySet()){
+            keys.add(userName);
+        }
 
         //show students
 
         System.out.println("Here are GitHub usernames of our students: ");
-        for (HashMap.Entry<String, String> entry: students.entrySet()){
-            System.out.println(" | " + entry.getValue() + " | ");
+//        for (HashMap.Entry<String, Student> entry: students.entrySet()){
+//            System.out.println(" | " + entry.getValue() + " | ");
+//        }
+        for(String userName : keys){
+            System.out.println("|" + userName + "|");
         }
 
         System.out.println("What student would you like to see more information on?? ");
          String userInput = scanner.nextLine();
 
 
-
-
-
-
+         if(students.containsKey(userInput)){
+             System.out.printf("%nStudent: %s", students.get(userInput).getName(), userInput);
+             System.out.printf("%nGrade Average: %.2f", students.get(userInput).getGradeAverage());
+         }
 
 
 
 
     }
+
+
+
+
 }
